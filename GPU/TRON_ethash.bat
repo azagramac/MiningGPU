f54c059d659d35222362052a849d7e5d16814a53
Jose l. Azagra <azagramac@gmail.com>
2021-04-19 10:25:38 +0200

    Update -mclock to 1805Mhz

--- GPU/TRON_ethash.bat
+++ GPU/TRON_ethash.bat
@@ -1,6 +1,5 @@
 TITLE PhoenixMiner v5.5c TRX (Tron)
 
-
 REM
 REM Mining TRX
 REM
@@ -11,6 +10,6 @@ setx GPU_USE_SYNC_OBJECTS 1
 setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
 
-PhoenixMiner.exe -pool ethash-eu.unmineable.com:13333 -pool2 ethash.unmineable.com:13333 -wal TRX:TMWRvSonbHyvjiLN2iE6YBRz1YXvvH3Gpk.worker#ggcx-9g8f -pass x -amd -mode 1 -clKernel 1 -clNew 1 -log 1  -ftime 55 -retrydelay 1 -tt 58 -tstop 89 -cclock 1250 -cvddc 750 -mclock 1800 -fanmin 55 -fanmax 70 -hstats 2 -prate 0.21
+PhoenixMiner.exe -pool ethash-eu.unmineable.com:13333 -pool2 ethash.unmineable.com:13333 -wal TRX:TMWRvSonbHyvjiLN2iE6YBRz1YXvvH3Gpk.worker#ggcx-9g8f -pass x -amd -mode 1 -clKernel 1 -clNew 1 -log 1  -ftime 55 -retrydelay 1 -tt 58 -tstop 89 -cclock 1250 -cvddc 750 -mclock 1805 -fanmin 55 -fanmax 70 -hstats 2 -prate 0.21
 pause
 
