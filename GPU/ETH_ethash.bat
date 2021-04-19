fd7f9d99eea879c27acd92dcce3b4cb18bef6f19
Jose l. Azagra <azagramac@gmail.com>
2021-04-19 10:25:20 +0200

    Update -mclock to 1805Mhz

--- GPU/ETH_ethash.bat
+++ GPU/ETH_ethash.bat
@@ -10,5 +10,5 @@ setx GPU_USE_SYNC_OBJECTS 1
 setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
 
-PhoenixMiner.exe -pool ethash-eu.unmineable.com:13333 -pool2 ethash.unmineable.com:13333 -wal ETH:0xf6d6071a91723580cb2bdcd8fd5336d3e55ed074.worker#ggcx-9g8f -pass x -amd -mode 1 -clKernel 1 -clNew 1 -log 1  -ftime 55 -retrydelay 1 -tt 58 -tstop 89 -cclock 1250 -cvddc 750 -mclock 1800 -fanmin 55 -fanmax 70 -hstats 2 -prate 0.21
+PhoenixMiner.exe -pool ethash-eu.unmineable.com:13333 -pool2 ethash.unmineable.com:13333 -wal ETH:0xf6d6071a91723580cb2bdcd8fd5336d3e55ed074.worker#ggcx-9g8f -pass x -amd -mode 1 -clKernel 1 -clNew 1 -log 1  -ftime 55 -retrydelay 1 -tt 58 -tstop 89 -cclock 1250 -cvddc 750 -mclock 1805 -fanmin 55 -fanmax 70 -hstats 2 -prate 0.21
 pause
